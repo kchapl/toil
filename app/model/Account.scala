112d33a8443f45e35146ec97d17b77eee9bb47f8
kchapl <kchapl@users.noreply.github.com>
2016-12-20 21:45:31 +0000

    Fix account balance calculation

--- app/model/Account.scala
+++ app/model/Account.scala
@@ -1,5 +1,7 @@
 package model
 
+import java.time.LocalDate
+
 import scala.annotation.tailrec
 
 case class Account(name: String, originalBalance: Amount, transactions: Set[Transaction]) {
@@ -24,6 +26,34 @@ case class Account(name: String, originalBalance: Amount, transactions: Set[Tran
       case (date, txs) => DateAmount(date, Amount.sum(txs.map(_.amount).toSeq: _*))
     }.toSeq.sortBy(_.date.toEpochDay)
 
-    go(diffs)
+    val filledDiffs = diffs.foldLeft(Seq.empty[DateAmount]) { (soFar, curr) =>
+      soFar ++ soFar.lastOption.map(a => filling(a, curr)).getOrElse(Nil) :+ curr
+    }
+
+    go(filledDiffs)
+  }
+
+  private def filling(a: DateAmount, b: DateAmount): Seq[DateAmount] = {
+
+    val end: LocalDate = b.date
+
+    @tailrec
+    def go(start: LocalDate, soFar: Seq[DateAmount]): Seq[DateAmount] = {
+      if (start == end) soFar
+      else {
+        soFar.lastOption match {
+          case Some(prev) =>
+            if (prev.date.plusDays(1).isBefore(end)) {
+              go(start.plusDays(1), soFar :+ DateAmount(start, Amount(0)))
+            } else {
+              soFar
+            }
+          case None =>
+            go(start.plusDays(1), Seq(DateAmount(start, Amount(0))))
+        }
+      }
+    }
+
+    go(a.date.plusDays(1), Nil)
   }
 }
